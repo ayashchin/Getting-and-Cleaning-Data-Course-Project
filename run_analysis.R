d719bca462d9e4b36d188cc1b02c25ec8dc3af89
Ashly Yashchin <ashly.yashchin@gmail.com>
2017-04-19 10:08:11 -0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,10 +1,10 @@
 #Here are the data for the project: https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
 
 #You should create one R script called run_analysis.R that does the following: 
-#Merges the training (17:28,13:24) and the test (5:16,1:12) sets to create one data set. - DONE
-#Extracts only the measurements on the mean and standard deviation for each measurement. - DONE
-#Uses descriptive activity names to name the activities in the data set - DONE
-#Appropriately labels the data set with descriptive variable names. - DONE
+#Merges the training and the test sets to create one data set.
+#Extracts only the measurements on the mean and standard deviation for each measurement.
+#Uses descriptive activity names to name the activities in the data set.
+#Appropriately labels the data set with descriptive variable names.
 #From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject. - DONE
   
 #Setting Up The Workspace
@@ -30,10 +30,8 @@ desired_names <- gsub('-mean','Mean',desired_names)
 desired_names <- gsub('-std','Std',desired_names)
 desired_names <- gsub('[-()]','',desired_names)
 
-# read txt files into a list (separator is a space)
+# read txt files into a list (separator is a space) & Convert to Data Tables
 data_list = lapply(HARzip[5:28], read.table)
-
-#Convert data to data tables
 data_tables <- lapply(data_list,tbl_df)
 
 #Merge Subject & Activity Data into New Sets
@@ -46,10 +44,9 @@ Subject_Train <- rename(data_tables[[22]],Subject=V1)
 Activity_Train <- rename(data_tables[[24]],Activity=V1)
 clean_train <- data_tables[[23]][desired_measures]
 clean_data_train <- cbind(Subject_Train,Activity_Train,clean_train)
-
-
-#Merge Into One Table & Add Labels
 clean_merge <- rbind(clean_data_test,clean_data_train)
+
+#Add Descriptive Labels
 colnames(clean_merge) <- c("Subject", "Activity", desired_names)
 clean_merge$Activity <- factor(clean_merge$Activity, levels = activity_variables[,1],labels=activity_variables[,2])
 clean_merge$Subject <- as.factor(clean_merge$Subject)
@@ -61,4 +58,6 @@ tidy_data <- rename(tidy_data,Variable=variable,Observation=value)
 tidy_data <- group_by(tidy_data,Subject,Activity,Variable)
 tidy_data <- summarize(tidy_data,mean(Observation))
 
-print(tidy_data)
+tidy_data_table <- write.table(tidy_data,file="tidy_data.csv")
+tidy_data_table <- read.table("tidy_data.csv")
+head(tidy_data_table)
